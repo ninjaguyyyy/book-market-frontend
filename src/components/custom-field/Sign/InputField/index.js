df1114756b63139dbef9eb533b31812e928e50a9
a3chi <ninjaguyyyy@gmail.com>
2020-06-09 15:00:49 +0700

    setting up formik + with InputField

---
+++ src/components/custom-field/Sign/InputField/index.js
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import PropTypes from "prop-types";
+import { Button, TextField, Grid } from "@material-ui/core";
+
+export default class InputFieldSign extends Component {
+    constructor(props) {
+        super(props);
+    }
+    static propTypes = {
+        field: PropTypes.object.isRequired,
+        form: PropTypes.object.isRequired,
+
+        type: PropTypes.string,
+        label: PropTypes.string,
+        placeholder: PropTypes.string,
+        disable: PropTypes.bool,
+    };
+
+    static defaultProps = {
+        type: "text",
+        label: "",
+        placeholder: "",
+        disable: false,
+    };
+
+    render() {
+        const { field, form, type, label, placeholder, disable } = this.props;
+        const { name, value, onChange, onBlur } = field;
+        return (
+            <div>
+                {type === "text" && (
+                    <TextField
+                        variant="outlined"
+                        margin="normal"
+                        required
+                        fullWidth
+                        id={name}
+                        label={label}
+                        autoComplete="email"
+                        autoFocus
+                        disabled={disable}
+                        {...field}
+                    />
+                )}
+            </div>
+        );
+    }
+}
--- src/features/User/components/LoginForm/index.js
+++ src/features/User/components/LoginForm/index.js
@@ -2,59 +2,73 @@ import React, { Component } from "react";
 import PropTypes from "prop-types";
 import { Link } from "react-router-dom";
 import { Button, TextField, Grid } from "@material-ui/core";
+import { Formik, Form, FastField } from "formik";
+
+import InputFieldSign from "../../../../components/custom-field/Sign/InputField";
 
 export default class LoginForm extends Component {
     static propTypes = {
         prop: PropTypes.func,
     };
 
+    constructor(props) {
+        super(props);
+        this.initialValues = {
+            email: "",
+        };
+    }
+
     render() {
         return (
-            <form className="form" noValidate>
-                <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    id="email"
-                    label="Địa chỉ email"
-                    name="email"
-                    autoComplete="email"
-                    autoFocus
-                />
-                <TextField
-                    variant="outlined"
-                    margin="normal"
-                    required
-                    fullWidth
-                    name="password"
-                    label="Mật khẩu"
-                    type="password"
-                    id="password"
-                    autoComplete="current-password"
-                />
-                <Button
-                    type="submit"
-                    fullWidth
-                    variant="contained"
-                    color="primary"
-                    className="submit"
-                >
-                    Xác nhận
-                </Button>
-                <Grid container>
-                    <Grid item xs>
-                        <Link href="#" variant="body2">
-                            Quên mật khẩu?
-                        </Link>
-                    </Grid>
-                    <Grid item>
-                        <Link to="/user/register" variant="body2">
-                            {"Bạn chưa có tài khoản? Đăng ký"}
-                        </Link>
-                    </Grid>
-                </Grid>
-            </form>
+            <Formik initialValues={this.initialValues}>
+                {(formikProps) => {
+                    // do something
+                    const { values, errors, touched } = formikProps;
+                    console.log(values);
+                    return (
+                        <Form className="form">
+                            <FastField
+                                name="email"
+                                component={InputFieldSign}
+                                label="Địa chỉ email"
+                                placeholder="Email"
+                            />
+                            <TextField
+                                variant="outlined"
+                                margin="normal"
+                                required
+                                fullWidth
+                                name="password"
+                                label="Mật khẩu"
+                                type="password"
+                                id="password"
+                                autoComplete="current-password"
+                            />
+                            <Button
+                                type="submit"
+                                fullWidth
+                                variant="contained"
+                                color="primary"
+                                className="submit"
+                            >
+                                Xác nhận
+                            </Button>
+                            <Grid container>
+                                <Grid item xs>
+                                    <Link href="#" variant="body2">
+                                        Quên mật khẩu?
+                                    </Link>
+                                </Grid>
+                                <Grid item>
+                                    <Link to="/user/register" variant="body2">
+                                        {"Bạn chưa có tài khoản? Đăng ký"}
+                                    </Link>
+                                </Grid>
+                            </Grid>
+                        </Form>
+                    );
+                }}
+            </Formik>
         );
     }
 }
