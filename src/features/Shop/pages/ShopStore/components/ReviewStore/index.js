88056677133ff8d90ff1f36432cc3323390355f0
a3chi <ninjaguyyyy@gmail.com>
2020-07-15 13:20:32 +0700

    component comment

--- src/features/Shop/pages/ShopStore/components/ReviewStore/index.js
+++ src/features/Shop/pages/ShopStore/components/ReviewStore/index.js
@@ -1,7 +1,39 @@
 import React from "react";
+import { Row, Col } from "reactstrap";
 
+import AddComment from "./components/AddComment";
+import Comment from "./components/Comment";
 import "./index.scss";
 
 export default function ReviewStore() {
-    return <div className="review-store">review store</div>;
+    return (
+        <div className="review-store">
+            <h5 className="mb-4">Nhận xét từ khách hàng:</h5>
+            <Row>
+                <Col xs={12}>
+                    <AddComment />
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12}>
+                    <Comment />
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12}>
+                    <Comment />
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12}>
+                    <Comment />
+                </Col>
+            </Row>
+            <Row>
+                <Col xs={12}>
+                    <Comment />
+                </Col>
+            </Row>
+        </div>
+    );
 }
